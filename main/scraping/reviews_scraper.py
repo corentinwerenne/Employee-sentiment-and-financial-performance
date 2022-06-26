9b6486b99bfcda6b3186b00b9e9f24cb2d4c7b87
corentinwerenne <corentinwerenne@gmail.com>
2022-06-26 19:01:18 +0200

    Fix regex

--- main/scraping/reviews_scraper.py
+++ main/scraping/reviews_scraper.py
@@ -12,6 +12,8 @@ import re #For regular expressions
 import codecs #To convert raw strings to normal ones
 import json #To convert a string to a dictionary
 import time
+import pandas as pd
+import random
 
 # Get credentials
 f = open('credentials.txt', 'r') #Opens the credentials.txt file for reading
@@ -46,7 +48,7 @@ def get_response(url):
     raw_response = repr(response.text)
     return raw_response
 
-response = get_response('https://www.glassdoor.com/Reviews/Deloitte-Reviews-E2763.htm')
+
 
 
 review_pattern = re.compile(r'{\"__typename\":\"EmployerReview\".+?\"translationMethod\":.+?}')
@@ -57,7 +59,7 @@ def get_reviews(response):
         reviews.append(match.group(0))
     return reviews
 
-reviews = get_reviews(response)
+
 
 def extract_from_review(review):
     """
@@ -66,33 +68,27 @@ def extract_from_review(review):
     review_id = re.compile(r'\"reviewId\":(\d+?),\"reviewDateTime"').search(review).group(1)
     review_datetime = re.compile(r'\"reviewDateTime\":\"(.+?)\",\"ratingOverall\"').search(review).group(1)
     rating_overall = re.compile(r'\"ratingOverall\":([0-5]),\"ratingCeo\"').search(review).group(1)
-    rating_ceo = re.compile(r'\"ratingCeo\":\"([A-Z]+)\",\"ratingBusinessOutlook\"').search(review).group(1)
-    rating_businessoutlook = re.compile(r'\"ratingBusinessOutlook\":\"([A-Z]+)\",\"ratingWorkLifeBalance\"').search(review).group(1)
+    rating_ceo = re.compile(r'\"ratingCeo\":(.+?),\"ratingBusinessOutlook\"').search(review).group(1)
+    rating_businessoutlook = re.compile(r'\"ratingBusinessOutlook\":(.+?),\"ratingWorkLifeBalance\"').search(review).group(1)
     rating_worklife_balance = re.compile(r'\"ratingWorkLifeBalance\":([0-5]),\"ratingCultureAndValues\"').search(review).group(1)
     rating_culture_values = re.compile(r'\"ratingCultureAndValues\":([0-5]),\"ratingDiversityAndInclusion\"').search(review).group(1)
     rating_diversity_inclusion = re.compile(r'\"ratingDiversityAndInclusion\":([0-5]),\"ratingSeniorLeadership\"').search(review).group(1)
     rating_senior_leadership = re.compile(r'\"ratingSeniorLeadership\":([0-5]),\"ratingRecommendToFriend\"').search(review).group(1)
-    rating_recommend_friend = re.compile(r'\"ratingRecommendToFriend\":\"([A-Z]+)\",\"ratingCareerOpportunities\"').search(review).group(1)
+    rating_recommend_friend = re.compile(r'\"ratingRecommendToFriend\":(.+?),\"ratingCareerOpportunities\"').search(review).group(1)
     rating_career_opport = re.compile(r'\"ratingCareerOpportunities\":([0-5]),\"ratingCompensationAndBenefits\"').search(review).group(1)
     rating_compensation_benefits = re.compile(r'\"ratingCompensationAndBenefits\":([0-5]),\"employer\"').search(review).group(1)
     is_current_job = re.compile(r'\"isCurrentJob\":([a-z]+),\"lengthOfEmployment\"').search(review).group(1)
     length_employment = re.compile(r'\"lengthOfEmployment\":(\d+),\"employmentStatus\"').search(review).group(1)
     employment_status = re.compile(r'"employmentStatus":\"([A-Z]+)\","jobEndingYear"').search(review).group(1)
     # Probably needs adjustement when a year is provided
-    job_ending_year = re.compile(r'"jobEndingYear":([a-z]+),"jobTitle"').search(review).group(1)
+    job_ending_year = re.compile(r'"jobEndingYear":(.+?),"jobTitle"').search(review).group(1)
     pros = re.compile(r'\"pros\":\"(.+?)\",\"prosOriginal\":').search(review).group(1)
     cons = re.compile(r'\"cons\":\"(.+?)\",\"consOriginal\":').search(review).group(1)
     advice = re.compile(r'\"advice\":(.+?),\"adviceOriginal\":').search(review).group(1)
     count_helpful = re.compile(r'\"countHelpful\":(\d+),\"countNotHelpful\":').search(review).group(1)
     count_nothelpful = re.compile(r'\"countNotHelpful\":(\d+),\"employerResponses\"').search(review).group(1)
     employer_reponses = re.compile(r'\"employerResponses\":(.+?),\"featured\"').search(review).group(1)
-    review_id = re.compile(r'\"languageId\":\"([a-z]+)\",\"translationMethod\"').search(review).group(1)
-    # review_id = re.compile(r'\"reviewId\":(\d+?),').search(review).group(1)
-    # review_id = re.compile(r'\"reviewId\":(\d+?),').search(review).group(1)
-    # review_id = re.compile(r'\"reviewId\":(\d+?),').search(review).group(1)
-    # review_id = re.compile(r'\"reviewId\":(\d+?),').search(review).group(1)
-    # review_id = re.compile(r'\"reviewId\":(\d+?),').search(review).group(1)
-    # review_id = re.compile(r'\"reviewId\":(\d+?),').search(review).group(1)
+    language_id = re.compile(r'\"languageId\":\"([a-z]+)\",\"translationMethod\"').search(review).group(1)
     
     review_data = {'review_id': review_id,
                'review_datetime': review_datetime,
@@ -116,8 +112,46 @@ def extract_from_review(review):
                'count_helpful': count_helpful,
                'count_nothelpful': count_nothelpful,
                'employer_reponses': employer_reponses,
-               'review_id': review_id}
+               'language_id': language_id}
     
     return review_data
 
-extract_from_review(response)
+
+def url_to_df(url, pages):
+    """
+
+    Parameters
+    ----------
+    url : TYPE
+        DESCRIPTION.
+    p : TYPE
+        DESCRIPTION.
+    s : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    review_pattern = re.compile(r'{\"__typename\":\"EmployerReview\".+?\"translationMethod\":.+?}')
+    
+    reviews_parsed = []
+    htm_index = url.index('.htm')
+    for p in range(pages + 1):
+        urlp = url[:htm_index] + '_P' + str(p) + '.htm'
+        print(urlp)
+        response = get_response(urlp)
+        print(response)
+        reviews = get_reviews(response)
+        print(reviews)
+        for review in reviews:
+            reviews_parsed.append(extract_from_review(review))
+        time.sleep(random.uniform(4, 7))
+        
+        
+    return reviews_parsed
+
+reviews_df = url_to_df('https://www.glassdoor.com/Reviews/Deloitte-Reviews-E2763.htm', 10)
+
+    
